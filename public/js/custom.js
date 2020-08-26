ef3bea05c4f7a4a6767e3a5e0af3022bb73e4aa5
Tran Hoang Lan <motden100@gmail.com>
2020-08-26 17:48:19 +0700

    finish home

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,3 +3,21 @@ function myFunction(x) {
   document.querySelector('.menu__nav').classList.toggle("active");
 
 }
+
+$(document).ready(function () {
+
+  $('#owl-demo').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: true,
+    nav: true,
+    navText: ["<img src='./images/icon-back.png'>", "<img src='./images/icon-next.png'>"],
+    items: 1,
+    //navigation: true, // Show next and prev buttons
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    singleItem: true,
+    autoplay: true,
+
+  })
+});
